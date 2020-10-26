0a2442f47684e4cf73ee53db7cf6eb660f6cb998
Sylvia <smora1992@gmail.com>
2020-10-26 16:33:03 +0400

    Fixes indentation and DOM ready function

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-$('#js-shopping-list-form').submit( function(){
-    const item = $('#shopping-list-entry').val();
-    $('.shopping-list').append('<li>' +
-    '<span class="shopping-item">'+item+'</span>'+
-    '<div class="shopping-item-controls">' +
-      '<button class="shopping-item-toggle">' +
-        '<span class="button-label">check</span>' +
-      '</button>'+
-      '<button class="shopping-item-delete">' +
-        '<span class="button-label">delete</span>' +
-      '</button>' +
-    '</div>' +
-  '</li>');
-    return false;
+function main() {
+  $("#js-shopping-list-form").submit(function (e) {
+    e.preventDefault();
+    const item = $("#shopping-list-entry").val();
+    $(".shopping-list").append(
+      `<li> 
+      <span class="shopping-item">${item}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle"><span class="button-label">check</span></button>
+        <button class="shopping-item-delete"><span class="button-label">delete</span></button>
+      </div>
+    </li>`
+    );
+  });
 
-})
-$(document).on('click', '.shopping-item-delete', function(){
-    $(this).closest('li').remove()
-$(document).on('click', '.shopping-item-toggle', function(){
-    $(this).closest('li').find('.shopping-item').toggleClass('shopping-Item_checked')
-})
+  $(document).on("click", ".shopping-item-delete", function () {
+    $(this).closest("li").remove();
+  });
 
-})
+  $(document).on("click", ".shopping-item-toggle", function () {
+    $(this)
+      .closest("li")
+      .find(".shopping-item")
+      .toggleClass("shopping-Item_checked");
+  });
+}
+
+$(main);
